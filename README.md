6115d5ea18bd743cbe45f804cc44d9059b2cc014
Nathan Price <gravityfargo@gmail.com>
2024-03-02 01:21:33 -0500

    management vm install

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Virtual-HPC-Cluster
 A comprehensive guide for setting up a virtualized HPC cluster for research, development, and education. Includes setup guides, configurations, and is open for community contributions. 
+
+1. Setup storage server.
+2. Put the variables.sh file in the administrator's home directory.
+3. Use setup.sh to set-up the main host and create the management virtual machine.
+4. 
---
+++ setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+######################################
+# Prepare the main host
+######################################
+sudo apt install -y zsh git curl wget whois
+
+curl https://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/variables.sh -o ~/variables.sh
+
+echo "source ~/variables.sh" >> ~/.bashrc && source ~/.bashrc
+echo "source ~/variables.sh" >> ~/.zshrc && source ~/.zshrc
+
+sudo mkdir -p /storage/vms/isos
+curl https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -o /storage/vms/isos/jammy-server-cloudimg-amd64.img
+
+######################################
+# Intall the Management VM
+######################################
+
+sudo mkdir /storage/vms/$MANAGEMENT_SERVER_HOSTNAME
+cd /storage/vms/$MANAGEMENT_SERVER_HOSTNAME
+
+sudo tee meta-data.yaml <<EOF
+instance-id: $MANAGEMENT_SERVER_HOSTNAME
+local-hostname: $MANAGEMENT_SERVER_HOSTNAME
+EOF
+
+export ADMIN_PASSWORD_HASH=$(echo $TEMP_ADMIN_PASSWORD | mkpasswd --method=SHA-512 --rounds=4096)
+
+sudo tee user-data.yaml <<EOF
+#cloud-config
+
+users:
+  - name: $ADMIN_USER
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    groups: sudo, wheel
+    ssh_authorized_keys:
+      - $SSH_PUBLIC_KEY
+    lock_passwd: false
+EOF
+
+sudo qemu-img create -b /storage/vms/isos/jammy-server-cloudimg-amd64.img -f qcow2 -F qcow2 $MANAGEMENT_SERVER_HOSTNAME-base.img 40G
+
+sudo virt-install \
+--name $MANAGEMENT_SERVER_HOSTNAME \
+--ram 4096 \
+--vcpus 4 \
+--import \
+--disk path=$MANAGEMENT_SERVER_HOSTNAME-base.img,format=qcow2 \
+--os-variant ubuntu22.04 \
+--network bridge=br0,model=virtio,mac=$MANAGEMENT_SERVER_MAC \
+--graphics vnc,listen=0.0.0.0 --noautoconsole \
+--cloud-init user-data=user-data.yaml,meta-data=meta-data.yaml \
+--debug
+
+# you can connect to the management server using ssh
+# or with
+sudo virsh console $MANAGEMENT_SERVER_HOSTNAME
