36252fef24f2aacef27baff493e51f5c51acbda4
Nathan Price <gravityfargo@gmail.com>
2024-03-05 21:23:56 -0500

    a few modifications to match the ansible playbook needs to be heavily reviewed

--- manual/storage.sh
+++ manual/storage.sh
@@ -2,10 +2,6 @@ sudo apt install -y liblua5.3-dev tcl-dev libreadline-dev nfs-kernel-server bc
 
 sudo tee -a /etc/exports <<EOF
 /storage        $SUBNET(rw,sync,no_subtree_check)
-/storage/projects   $SUBNET(rw,sync,no_subtree_check)
-/storage/home   $SUBNET(rw,sync,no_subtree_check)
-/storage/sw $SUBNET(rw,sync,no_subtree_check)
-/storage/spack  $SUBNET(rw,sync,no_subtree_check)
 EOF
 
 sudo systemctl enable nfs-kernel-server
@@ -16,37 +12,32 @@ sudo exportfs -a
 ######################################
 # Lmod
 ######################################
-sudo su - swmanager
-cd /tmp && wget https://github.com/TACC/Lmod/archive/refs/tags/8.7.34.tar.gz
-tar -xzvf 8.7.34.tar.gz
+sudo su - spack
+cd /tmp && wget https://github.com/TACC/Lmod/archive/refs/tags/$LMOD_VERSION.tar.gz
+tar -xzvf $LMOD_VERSION.gz
 cd Lmod-8.7.34
-./configure --prefix=/storage/sw
+./configure --prefix=/storage/software
 make install
-sudo ln -s /storage/sw/lmod/lmod/init/profile /etc/profile.d/z00_lmod.sh
+sudo ln -s /storage/software/lmod/lmod/init/profile /etc/profile.d/z00_lmod.sh
 
-echo 'MODULEPATH="/storage/sw/modules/linux-ubuntu22.04-x86_64/Core"' | sudo tee -a /etc/environment > /dev/null
+echo 'MODULEPATH="/storage/software/modules/linux-ubuntu22.04-x86_64/Core"' | sudo tee -a /etc/environment > /dev/null
 exit
-module purge
-clearMT
-unset MODULEPATH
-module use /storage/sw/modules/linux-ubuntu22.04-x86_64/Core
-
 ######################################
 # spack
 ######################################
-sudo su - swmanager
+sudo su - spack
 cd /tmp && wget https://github.com/spack/spack/releases/download/v0.21.0/spack-0.21.0.tar.gz
 tar -xzvf spack-0.21.0.tar.gz
 mv spack-0.21.0/* /storage/spack/
 echo "source /storage/spack/share/spack/setup-env.sh" >> ~/.bashrc
 source ~/.bashrc
-# for other users add them to the swmanager group and add the source line to their .bashrc/.zshrc
-vim /storage/spack/etc/spack/config.yaml
-# see storage\config.yaml
+
+curl ttps://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/assets/spack/config.yaml -o /storage/spack/etc/spack/config.yaml
+curl https://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/assets/spack/modules.yaml -o /storage/spack/etc/spack/modules.yaml
+curl https://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/assets/spack/compilers.yaml -o /storage/spack/etc/spack/compilers.yaml
 vim /storage/spack/etc/spack/modules.yaml
-# see storage\modules.yaml
-# tells lmod where spack installed packages
-echo '/storage/sw/modules/linux-ubuntu22.04-x86_64/Core' > /storage/sw/lmod/lmod/init/.modulespath
+
+echo '/storage/software/modules/linux-ubuntu22.04-x86_64/Core' > /storage/software/lmod/lmod/init/.modulespath
 # example packages install
 spack install miniconda3 py-numpy py-tensorflow r-dplyr py-pandas tar
 reboot
