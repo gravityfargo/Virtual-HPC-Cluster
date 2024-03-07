809b3256cbeba37963f60ed30075326add8f8179
Nathan Price <gravityfargo@gmail.com>
2024-03-07 12:39:59 -0500

    fixed .variable path, added some comments, reordered vm commands

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -3,9 +3,10 @@
 # Setup Packages on Management Server
 ######################################
 sudo apt update -y && sudo apt -y upgrade && \
-sudo apt install -y zsh git curl wget whois
-chmod 600 ~/variables.sh
-echo "source ~/.variables.sh" >> ~/.bashrc && source ~/.bashrc
+sudo apt install -y zsh git curl wget whois && \
+sudo mv ~/.variables.sh /.variables.sh && \
+chmod 600 /.variables.sh && \
+echo "source /.variables.sh" >> ~/.bashrc && source ~/.bashrc
 
 sudo sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config && \
 sudo systemctl restart sshd
@@ -16,7 +17,7 @@ sudo apt-add-repository ppa:ansible/ansible && \
 sudo apt install -y ansible
 
 sudo groupadd -r ansibleadmins && \
-sudo usermod -aG ansibleadmins $ADMIN_USER && \
+sudo usermod -aG ansibleadmins $USER && \
 exec sudo su -l $USER
 
 sudo chown -R root:ansibleadmins /etc/ansible && \
@@ -50,8 +51,8 @@ EOF
 
 ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519 && \
 SSH_KEY_CONTENT=$(cat ~/.ssh/id_ed25519.pub) && \
-echo export SSH_PUBLIC_KEY_MGMT=\"$SSH_KEY_CONTENT\" >> ~/.variables.sh && \
-source ~/.bashrc && source ~/.zshrc
+echo export SSH_PUBLIC_KEY_MGMT=\"$SSH_KEY_CONTENT\" >> /.variables.sh && \
+source ~/.bashrc
 
 echo $SSH_PUBLIC_KEY_MGMT >> ~/.ssh/authorized_keys
 
@@ -59,6 +60,7 @@ ssh-keyscan $STORAGE_SERVER_HOSTNAME >> ~/.ssh/known_hosts && \
 ssh-keyscan $MANAGEMENT_SERVER_HOSTNAME >> ~/.ssh/known_hosts
 
 ssh-copy-id $STORAGE_SERVER_HOSTNAME
+# perform the same for any non vm hosts
 
 ansible all -m ping
 
@@ -74,19 +76,19 @@ ansible-playbook create-vm.yml -e "hostname=$LOGIN_SERVER_HOSTNAME" \
 -e "ssh_public_key_mgmt='$SSH_PUBLIC_KEY_MGMT'" \
 -e "ip=$LOGIN_SERVER_IP"
 
-# ansible-playbook create-vm.yml -e "hostname=$WORKER_SERVER_HOSTNAME" \
-# -e "vm_host='$WORKER_SERVER_HOSTNAME'" \
-# -e "admin_user=$ADMIN_USER" -e "mac=$WORKER_SERVER_MAC" \
-# -e "ssh_public_key_personal='$SSH_PUBLIC_KEY_PERSONAL'" \
-# -e "ssh_public_key_mgmt='$SSH_PUBLIC_KEY_MGMT'" \
-# -e "ip=$WORKER_SERVER_IP"
-
 ansible-playbook create-vm.yml -e "hostname=$HEAD_SERVER_HOSTNAME" \
 -e "vm_host='$STORAGE_SERVER_HOSTNAME'" \
 -e "admin_user=$ADMIN_USER" -e "mac=$HEAD_SERVER_MAC" \
 -e "ssh_public_key_personal='$SSH_PUBLIC_KEY_PERSONAL'" \
 -e "ssh_public_key_mgmt='$SSH_PUBLIC_KEY_MGMT'" \
 -e "ip=$HEAD_SERVER_IP"
+
+# ansible-playbook create-vm.yml -e "hostname=$WORKER_SERVER_HOSTNAME" \
+# -e "vm_host='$WORKER_SERVER_HOSTNAME'" \
+# -e "admin_user=$ADMIN_USER" -e "mac=$WORKER_SERVER_MAC" \
+# -e "ssh_public_key_personal='$SSH_PUBLIC_KEY_PERSONAL'" \
+# -e "ssh_public_key_mgmt='$SSH_PUBLIC_KEY_MGMT'" \
+# -e "ip=$WORKER_SERVER_IP"
 
 ######################################
 # Delete VMs
@@ -102,11 +104,11 @@ curl https://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/pla
 # do not use the "all" as host until after running these commands for the first time.
 curl https://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/playbooks/prepare-base-os.yml -o ~/prepare-base-os.yml
 
-ansible-playbook prepare-base-os.yml -e "target_hostname=$STORAGE_SERVER_HOSTNAME" -e "admin_user=$ADMIN_USER"
 ansible-playbook prepare-base-os.yml -e "target_hostname=$MANAGEMENT_SERVER_HOSTNAME" -e "admin_user=$ADMIN_USER"
+ansible-playbook prepare-base-os.yml -e "target_hostname=$STORAGE_SERVER_HOSTNAME" -e "admin_user=$ADMIN_USER"
 ansible-playbook prepare-base-os.yml -e "target_hostname=$LOGIN_SERVER_HOSTNAME" -e "admin_user=$ADMIN_USER"
-ansible-playbook prepare-base-os.yml -e "target_hostname=$WORKER_SERVER_HOSTNAME" -e "admin_user=$ADMIN_USER"
 ansible-playbook prepare-base-os.yml -e "target_hostname=$HEAD_SERVER_HOSTNAME" -e "admin_user=$ADMIN_USER"
+ansible-playbook prepare-base-os.yml -e "target_hostname=$WORKER_SERVER_HOSTNAME" -e "admin_user=$ADMIN_USER"
 
 ######################################
 # Prepare the Storage Server
@@ -114,13 +116,25 @@ ansible-playbook prepare-base-os.yml -e "target_hostname=$HEAD_SERVER_HOSTNAME" 
 curl https://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/playbooks/prepare-storage-server.yml -o ~/prepare-storage-server.yml
 
 ansible-playbook prepare-storage-server.yml \
--e "storage_server_hostname=$LOGIN_SERVER_HOSTNAME" \
+-e "storage_server_hostname=$STORAGE_SERVER_HOSTNAME" \
 -e "subnet=$SUBNET" \
 -e "lmod_version=$LMOD_VERSION" \
 -e "admin_user=$ADMIN_USER"
 
-# you will need to `sudo su spack` then `spack install tar` or some package to get spack to work the first time.
+# Login to the storage server and run the following commands
+sudo su spack
+spack install gmake
+
+# ansible-playbook undo-storage.yml \
+# -e "storage_server_hostname=$STORAGE_SERVER_HOSTNAME" \
+# -e "subnet=$SUBNET"
 
 ######################################
 # Prepare the Login, Worker, and Head Servers
 ######################################
+curl https://raw.githubusercontent.com/gravityfargo/Virtual-HPC-Cluster/main/playbooks/prepare-hpc-clients.yml -o ~/prepare-hpc-clients.yml
+
+ansible-playbook prepare-hpc-clients.yml \
+-e target_hostname=$LOGIN_SERVER_HOSTNAME \
+-e "storage_server_hostname=$STORAGE_SERVER_HOSTNAME" \
+-e "admin_user=$ADMIN_USER"
